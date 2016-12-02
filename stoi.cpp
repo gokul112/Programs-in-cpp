0317fb11db34e28e17c96b86e3c2068962704996
gokul112 <gokulraj112@gmail.com>
2016-12-02 10:55:06 +0530

    Add files via upload

---
+++ stoi.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <string.h>
+using namespace std;
+
+int main()
+{
+    std::string si;
+    std::string sf;
+    cin>>si;
+
+    int i = std::stoi(si);
+
+
+    std::cout << i << std::endl;
+
+    return 0;
+}
